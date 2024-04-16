4b9975e7a2bfdf402804ae466627535ec1961b34
CrazyBobcatStudios <160531044+CrazyBobcatStudios@users.noreply.github.com>
2024-04-15 20:46:31 -0500

    Add project files.

---
+++ Console.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Type help or enter a command");
+
+string? input = Console.ReadLine();
+
+if (input != null)
+{
+    input = input.ToLower();
+    if (input.StartsWith("help"))
+    {
+        Console.WriteLine("No");
+    }
+
+    if (input.StartsWith("yeet"))
+    {
+        Console.WriteLine("Ye");
+        Console.WriteLine("et");
+    }
+
+    if (input.StartsWith("bctxt"))
+    {
+
+    }
+
+}
+
+else
+{
+    Console.WriteLine("Error: Null Input");
+}
